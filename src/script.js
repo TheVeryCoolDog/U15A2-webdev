06dfba958ad1b024f4304004e4ec98b84ffd899e
VeryCoolDog <verycooldog@hotmail.com>
2025-05-23 04:18:17 +0100

    Began apply page Fixed card related scaling issue on mobile

--- src/script.js
+++ src/script.js
@@ -1,18 +1,6 @@
 function HideShow(val) {
-    let element;
-    switch (val) {
-        case "daniel":
-            element = document.getElementById('daniel_text');
-            break
-        case "li":
-            element = document.getElementById('li_text');
-            break;
-        case "rosa":
-            element = document.getElementById('rosa_text');
-            break;
-        default:
-            break;
-    }
+    let element = document.getElementById(val);
+
     if (element.classList.contains("d-none")) {
         element.classList.remove("d-none");
         element.classList.add("d-block");
